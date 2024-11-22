94d703db8c5bb001709c42267c13035b76fb929b
Haoye Chen <149187979+leaf-down@users.noreply.github.com>
2024-11-22 15:40:10 +0800

    updata

--- train.py
+++ train.py
@@ -16,11 +16,13 @@ def main():
     print("using {} device.".format(device))
 
     data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+        "train": transforms.Compose([transforms.Grayscale(num_output_channels=3),
+                                     transforms.RandomResizedCrop(224),
                                      transforms.RandomHorizontalFlip(),
                                      transforms.ToTensor(),
                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-        "val": transforms.Compose([transforms.Resize((224, 224)),
+        "val": transforms.Compose([transforms.Grayscale(num_output_channels=3),
+                                   transforms.Resize((224, 224)),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
